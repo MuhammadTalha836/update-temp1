c6ce26ec4c8dcba777490d4e835e9efba6b8262c
Mian Talha <muhammadtalhatalha425@gamil.com>
2023-01-16 21:14:42 +0500

    changing

--- src/components/ServiceTop.js
+++ src/components/ServiceTop.js
@@ -28,7 +28,7 @@ export default function ServiceTop() {
                 </div>
 
             </div>
-                <div className='whatsApp__button' data-aos="fade-up" data-aos-duration="800" style={{marginTop:"140px"}}>
+                <div className='whatsApp__button' data-aos="fade-up" data-aos-duration="800" style={{marginTop:"190px"}}>
                     <a target="_blank" href="" style={{ backgroundColor: 'rgb(240, 240, 60)', width: '240px' }} className="btn px-4 py-3 text-white d-flex align-items-center justify-content-between">
                         <span>Contact Now</span>
                         <BsWhatsapp color='white' size={28} className="whatsApp__icon" />
